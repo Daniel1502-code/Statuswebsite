23bde6215f91eb8b46191d6ac94bb6812cf18488
DanielDuesenDieb <110566119+DanielDuesenDieb@users.noreply.github.com>
2023-09-19 18:07:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
-import requests
 import datetime
-import sqlite3
+import json
 
 app = Flask(__name__)
 
+with open('data.json') as f:
+    data = json.load(f)
+
+# Define all the global variables with initial values
+google_name = google_hostname = google_status = google_response_code = google_response_time_ms = ""
+youtube_name = youtube_hostname = youtube_status = youtube_response_code = youtube_response_time_ms = ""
+steam_name = steam_hostname = steam_status = steam_response_code = steam_response_time_ms = ""
+twitter_name = twitter_hostname = twitter_status = twitter_response_code = twitter_response_time_ms = ""
+test_name = test_hostname = test_status = test_response_code = test_response_time_ms = ""
 
 @app.route("/")
 def main():
@@ -14,60 +22,91 @@ def main():
     url_steam = "https://store.steampowered.com/"
     url_test = "http://danielduesendieb.pythonanywhere.com"
 
-    r = requests.get(url_google)
-    name1 = r.status_code
-    r = requests.get(url_youtube)
-    name2 = r.status_code
-    r = requests.get(url_steam)
-    name3 = r.status_code
-    r = requests.get(url_twitter)
-    name4 = r.status_code
-    r = requests.get(url_test)
-    test = r.status_code
-
     def downtime(website):
         now = datetime.datetime.now()
         print(website, "is Down... Datetime", now)
 
-    if name1 != 200:
-        downtime(url_google)
-    elif name2 != 200:
-        downtime(url_youtube)
-    elif name3 != 200:
-        downtime(url_steam)
-    elif name4 != 200:
-        downtime(url_twitter)
-    elif test != 200:
-        downtime(url_test)
-    else:
-        print("Everything is Running!")
+    def jsonsearch():
+        global google_name, google_hostname, google_status, google_response_code, google_response_time_ms
+        global youtube_name, youtube_hostname, youtube_status, youtube_response_code, youtube_response_time_ms
+        global steam_name, steam_hostname, steam_status, steam_response_code, steam_response_time_ms
+        global twitter_name, twitter_hostname, twitter_status, twitter_response_code, twitter_response_time_ms
+        global test_name, test_hostname, test_status, test_response_code, test_response_time_ms
 
-    downtime = datetime.datetime.now()
+        for server in data["web_servers"]:
+            if server["name"] == "google":
+                google_name = server["name"]
+                google_hostname = server["hostname"]
+                google_status = server["status"]
+                google_response_code = server["response_code"]
+                google_response_time_ms = server["response_time_ms"]
+            if server["name"] == "youtube":
+                youtube_name = server["name"]
+                youtube_hostname = server["hostname"]
+                youtube_status = server["status"]
+                youtube_response_code = server["response_code"]
+                youtube_response_time_ms = server["response_time_ms"]
+            if server["name"] == "steam":
+                steam_name = server["name"]
+                steam_hostname = server["hostname"]
+                steam_status = server["status"]
+                steam_response_code = server["response_code"]
+                steam_response_time_ms = server["response_time_ms"]
+            if server["name"] == "twitter":
+                twitter_name = server["name"]
+                twitter_hostname = server["hostname"]
+                twitter_status = server["status"]
+                twitter_response_code = server["response_code"]
+                twitter_response_time_ms = server["response_time_ms"]
+            if server["name"] == "test":
+                test_name = server["name"]
+                test_hostname = server["hostname"]
+                test_status = server["status"]
+                test_response_code = server["response_code"]
+                test_response_time_ms = server["response_time_ms"]
+
+    jsonsearch()
+
     return render_template(
         "status.html",
-        name1=name1,
-        name2=name2,
-        name3=name3,
-        name4=name4,
-        test=test,
-        downtime=downtime,
+        test_name=test_name,
+        test_hostname=test_hostname,
+        test_status=test_status,
+        test_response_code=test_response_code,
+        test_response_time_ms=test_response_time_ms,
+        twitter_name=twitter_name,
+        twitter_hostname=twitter_hostname,
+        twitter_status=twitter_status,
+        twitter_response_code=twitter_response_code,
+        twitter_response_time_ms=twitter_response_time_ms,
+        steam_name=steam_name,
+        steam_hostname=steam_hostname,
+        steam_status=steam_status,
+        steam_response_code=steam_response_code,
+        steam_response_time_ms=steam_response_time_ms,
+        youtube_name=youtube_name,
+        youtube_hostname=youtube_hostname,
+        youtube_status=youtube_status,
+        youtube_response_code=youtube_response_code,
+        youtube_response_time_ms=youtube_response_time_ms,
+        google_name=google_name,
+        google_hostname=google_hostname,
+        google_status=google_status,
+        google_response_code=google_response_code,
+        google_response_time_ms=google_response_time_ms,
     )
-
 
 @app.route("/edit")
 def edit():
     return "Still in construction!"
 
-
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
 
-
 @app.route("/about")
 def about():
     return render_template("about.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
